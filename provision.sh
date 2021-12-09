c4d55adb96e2c8c2ff228d7e99b9f194e5893bee
Delwar35 <94615905+Delwar35@users.noreply.github.com>
2021-12-09 17:21:59 +0000

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -17,6 +17,7 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
 
 #install pm2
+sudo apt install npm -y
 sudo npm install pm2 -g
 
 # 192.168.10.100:3000 to access on browser
